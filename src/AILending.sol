cdde55bfd469e0924e946e60b73f83ec0f2f498e
2PykeETH <ColePyke98@outlook.com>
2025-10-13 16:05:46 -0700

    feat: Add Contract Title Natspec Comment

--- src/AILending.sol
+++ src/AILending.sol
@@ -5,8 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-//import "./interfaces/ILendingPool.sol";
+//import "./interfaces/IAILending.sol";
 //import "./LendingPoolToken.sol";
 
+/**
+ * @title AILending
+ * @notice A simple lending pool where users can deposit tokens to earn yield and borrow against collateral
+ * @dev Implements a basic lending/borrowing mechanism with interest accrual
+ */
 contract AILending {}
-    
