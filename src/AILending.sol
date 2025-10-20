a1feae3f67f174847915c4ae9b6cce17a4bbb349
2PykeETH <ColePyke98@outlook.com>
2025-10-20 11:25:56 -0700

    feat: Create IAILending Interface

--- src/AILending.sol
+++ src/AILending.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-//import "./interfaces/IAILending.sol";
+import "./interfaces/IAILending.sol";
 //import "./LendingPoolToken.sol";
 
 /**
@@ -13,7 +13,7 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
  * @notice A simple lending pool where users can deposit tokens to earn yield and borrow against collateral
  * @dev Implements a basic lending/borrowing mechanism with interest accrual
  */
-contract AILending is ILendingPool, Ownable, ReentrancyGuard {
+contract AILending is IAILendingPool, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     // State variables
