dc2321f0228ce456adeb7d69bb9fbfcef88985e4
2PykeETH <ColePyke98@outlook.com>
2025-10-22 11:56:53 -0700

    feat: forge fmt

--- src/interfaces/IAILending.sol
+++ src/interfaces/IAILending.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.8.19;
  * @notice Interface for the lending pool contract
  */
 interface IAILending {
-    
     // Structs
     struct UserInfo {
         uint256 collateralBalance;
@@ -17,10 +16,11 @@ interface IAILending {
     // Events
     event Deposit(address indexed user, uint256 amount, uint256 lpTokens);
     event Withdraw(address indexed user, uint256 amount, uint256 lpTokens);
-        event DepositCollateral(address indexed user, uint256 amount);
-            event WithdrawCollateral(address indexed user, uint256 amount);
-                event Borrow(address indexed user, uint256 amount);
-event Repay(address indexed user, uint256 amount);
-event Liquidation(address indexed liquidator, address indexed borrower, uint256 collateralSeized, uint256 debtRepaid);
+    event DepositCollateral(address indexed user, uint256 amount);
+    event WithdrawCollateral(address indexed user, uint256 amount);
+    event Borrow(address indexed user, uint256 amount);
+    event Repay(address indexed user, uint256 amount);
+    event Liquidation(
+        address indexed liquidator, address indexed borrower, uint256 collateralSeized, uint256 debtRepaid
+    );
 }
-
