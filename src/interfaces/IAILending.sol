abfddececb75307f0896d5894e7f3f46f94a51f5
2PykeETH <ColePyke98@outlook.com>
2025-10-22 11:55:53 -0700

    feat: Add Borrow Event In IAILending Interface

--- src/interfaces/IAILending.sol
+++ src/interfaces/IAILending.sol
@@ -19,5 +19,5 @@ interface IAILending {
     event Withdraw(address indexed user, uint256 amount, uint256 lpTokens);
         event DepositCollateral(address indexed user, uint256 amount);
             event WithdrawCollateral(address indexed user, uint256 amount);
-            
+                event Borrow(address indexed user, uint256 amount);
 }
