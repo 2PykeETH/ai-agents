a078e1bcc2e6f922d07fc9a69facb876da30bb02
2PykeETH <ColePyke98@outlook.com>
2025-10-24 09:05:36 -0700

    feat: Add borrow External Function

--- src/interfaces/IAILending.sol
+++ src/interfaces/IAILending.sol
@@ -23,5 +23,6 @@ interface IAILending {
         function withdraw(uint256 lpTokenAmount) external;
     function depositCollateral(uint256 amount) external;
     function withdrawCollateral(uint256 amount) external;
+    function borrow(uint256 amount) external;
 
 }
