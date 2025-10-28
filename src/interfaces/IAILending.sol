bce8b9cd5f09b62720fdf5e68c08a25a88d94853
2PykeETH <ColePyke98@outlook.com>
2025-10-28 11:15:13 -0700

    feat: forge fmt

--- src/interfaces/IAILending.sol
+++ src/interfaces/IAILending.sol
@@ -37,6 +37,5 @@ interface IAILending {
     // State variables getters
     function totalDeposits() external view returns (uint256);
     function totalBorrows() external view returns (uint256);
-        function borrowIndex() external view returns (uint256);
-
+    function borrowIndex() external view returns (uint256);
 }
