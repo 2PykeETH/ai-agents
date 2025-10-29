e12f33c11676591457090057be79823928dae320
2PykeETH <ColePyke98@outlook.com>
2025-10-29 11:26:17 -0700

    feat: Add Contract Title Natspec & Contract Inheritance

--- src/mock/MockERC20.sol
+++ src/mock/MockERC20.sol
@@ -3,4 +3,9 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockERC20 {}
+/**
+ * @title MockERC20
+ * @notice Simple ERC20 token for testing purposes
+ * @dev Allows anyone to mint tokens for testing
+ */
+contract MockERC20 is ERC20 {}
